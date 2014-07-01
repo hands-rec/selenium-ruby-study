1c166cfffae6b80ac7640e90d6e35c1c75656cf2
hands-rec <hands.records@gmail.com>
2014-07-02 00:33:06 +0900

    change browser

--- selenium-scraping/scraping.rb
+++ selenium-scraping/scraping.rb
@@ -2,7 +2,7 @@
 
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :chrome
+driver = Selenium::WebDriver.for :firefox
 
 driver.navigate.to "http://google.com"
 element = driver.find_element(:name, "q")
@@ -10,5 +10,5 @@ element.send_keys("selenium ruby")
 element.submit
 
 puts driver.title
-
+sleep 1
 driver.quit
