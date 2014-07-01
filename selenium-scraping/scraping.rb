bd2e0876fb4454091b400461d195b0baa7eee818
hands-rec <hands.records@gmail.com>
2014-07-01 20:00:48 +0900

    add google search

--- selenium-scraping/scraping.rb
+++ selenium-scraping/scraping.rb
@@ -5,5 +5,10 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
 
 driver.navigate.to "http://google.com"
+element = driver.find_element(:name, "q")
+element.send_keys("selenium ruby")
+element.submit
+
+puts driver.title
 
 driver.quit
